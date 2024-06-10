bbdf86a1956d89710065c107ed4264e09d170f58
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-06-10 23:42:51 +0600

    add express

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // const express = require('express')
 const cors = require('cors')
 const app = express()
-require('dotenv').config()
+// require('dotenv').config()
 var jwt = require('jsonwebtoken');
 const stripe = require("stripe")(`${process.env.PAYMENT_KEY}`);
 const port =process.env.PORT || 3000
