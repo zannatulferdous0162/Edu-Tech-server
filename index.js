e7a4cb38378e9db312e0e2543df386145c28c03a
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-06-10 23:43:53 +0600

    addport

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-// require('dotenv').config()
+require('dotenv').config()
 var jwt = require('jsonwebtoken');
 const stripe = require("stripe")(`${process.env.PAYMENT_KEY}`);
-const port =process.env.PORT || 3000
+// const port =process.env.PORT || 3000
 app.use(express.json())
 app.use(cors())
 
